1bdb53ceee21b3cc1fb9c1b3a373375b15953c50
Colin Tang <53540424+cot2005@users.noreply.github.com>
2020-10-02 14:20:02 -0400

    Update RNAseqPipeline.R
    Updated aesthetics of volcano plot. Added Geneset file input for labeling. Added x and y limits. Converts outliers (outside input limits) to triangles. Significance is defined by alpha value and LogFC input limits.

--- RNAseqPipeline.R
+++ RNAseqPipeline.R
@@ -134,17 +134,38 @@ RNApipeline.DESeq2<-function(tempRNAdata, conditions, geneKey) {
 }
 
 
-#makes volcanoplot
-RNApipeline.volcano.plot<-function(datadf, alphavalue = 0.05, topgenes = 20, graphname = "Volcanoplot") {
+# Makes volcanoplot
+#
+RNApipeline.volcano.plot<-function(rnaseqdata, alphavalue = 0.05, lfcLim = 1.5, topgenes = 10, genesetFile = NULL, graphname = "volcano_",
+                       xlims = c(-2.5,2.5), ylims = c(0,20), width = 6, height = 6) {
+  datadf$GeneSymbol <- as.character(datadf$GeneSymbol)
+  datadf$padj <- p.adjust(datadf$pvalue, method = "BH")
   datadf <- na.omit(datadf)
-  input <- mutate(datadf, sig = ifelse(datadf$padj < alphavalue, paste("FDR<",alphavalue, sep = ""), "Not Sig")) #Will have different colors depending on significance
-  v <- ggplot(input, aes(log2FoldChange, -log10(pvalue))) #volcanoplot with log2Foldchange versus pvalue
-  v + geom_point(aes(col = sig)) + #add points colored by significance
-    scale_color_manual(values = c("red","black")) + 
-    ggtitle(paste(graphname, "Volcano Plot")) + #e.g. 'Volcanoplot DESeq2'
-    geom_text_repel(data = head(input, 20), aes(label = GeneSymbol)) #adding text for the top 20 genes
-  ggsave(paste(graphname, "_volcano.pdf", sep = ""), width = 8,height = 6) #In case you want to easily save to disk
+  # manages gene labels
+  input <- mutate(datadf, sig = ifelse(datadf$padj < alphavalue & abs(datadf$log2FoldChange) >= lfcLim, "Sig", "Not_Sig")) #Will have different colors depending on significance
+  input$label <- ""
+  if (is.null(genesetFile) == F) {
+    geneset <- read.table(genesetFile, stringsAsFactors = F)
+    geneset$rownum <- match(geneset[,1], datadf$GeneSymbol)
+    input[geneset[,2],length(input)] <- as.character(geneset[,1])
+  } else if (top > 0 && is.null(genesetFile) == T) {
+    input$label[1:top] <- input$GeneSymbol[1:top]
+  }
+  # changes outliers to triangles
+  input$shape <- ifelse(-log10(input$padj) > ylims[2] | abs(input$log2FoldChange) > xlims[2], "triangle", "circle")
+  input$padj[-log10(input$padj) > ylims[2]] <- 10^-(ylims[2])
+  input$log2FoldChange[input$log2FoldChange > xlims[2]] <- xlims[2]
+  input$log2FoldChange[input$log2FoldChange < -xlims[2]] <- -xlims[2]
+  # begins plotting
+  v <- ggplot(input, aes(log2FoldChange, -log10(padj))) #volcanoplot with log2Foldchange versus pvalue
+  v + geom_point(aes(col = sig, shape=shape)) + #add points colored by significance
+    scale_color_manual(values = c("grey50","red")) + xlim(xlims) + ylim(ylims) +
+    xlab("log2(fold change)") + ylab("-log10(FDR)") + theme_bw() + 
+    theme(legend.position = "none", axis.text = element_text(size=14), axis.title = element_text(size=16,face="bold")) + #ggtitle("Volcanoplot")
+    geom_text_repel(label = input$label, box.padding = 0.3, size = 4, force = 9, segment.size = 0.3, min.segment.length = 0.1, segment.alpha = 0.5) #adding text for the top 20 genes
+  ggsave(paste(graphname, "_Volcanoplot.pdf", sep = ""), width = width, height = height)
 }
+
 
 #expects data with no gene names or IDs as columns
 RNApipeline.pca<-function(pcaData, pcaGroups) {
