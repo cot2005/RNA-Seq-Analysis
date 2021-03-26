c0cab9fdfbfbccac5b0606234c0fc99c5c2141a6
Colin Tang <53540424+cot2005@users.noreply.github.com>
2021-03-26 09:05:25 -0700

    Update VolcanoPlot.R

--- VolcanoPlot.R
+++ VolcanoPlot.R
@@ -5,8 +5,8 @@ library("dplyr")
 # volcano plot function used to make custom volcano plots and label genes of interest.
 
 volcano.plot<-function(rnaseqdata, alphavalue = 0.05, lfcLim = 1.5, top = 10, genesetFile = NULL, graphname = "volcano",
-                       xlims = c(-2.5,2.5), ylims = c(0,20), width = 6, height = 6, 
-                       boxpadding = 0.5, labelRepel = 10, labelSize = 5, axisLabelsize = 16, axisTitle = 18) {
+                       xlims = c(-2.5,2.5), ylims = c(0,20), width = 6, height = 6, pointsize = 1.5,
+                       boxpadding = 0.5, labelRepel = 10, labelSize = 5, axisLabelsize = 12, axisTitle = 14) {
   datadf <- read.table(rnaseqdata, sep = "\t", header = T)
   datadf$GeneSymbol <- as.character(datadf$GeneSymbol)
   datadf$padj <- p.adjust(datadf$pvalue, method = "BH")
@@ -29,7 +29,7 @@ volcano.plot<-function(rnaseqdata, alphavalue = 0.05, lfcLim = 1.5, top = 10, ge
   input$log2FoldChange[input$log2FoldChange < -xlims[2]] <- -xlims[2]
   # begins plotting
   v <- ggplot(input, aes(log2FoldChange, -log10(padj))) #volcanoplot with log2Foldchange versus pvalue
-  v + geom_point(aes(col = sig, shape=shape)) + #add points colored by significance
+  v + geom_point(aes(col = sig, shape=shape), size = pointsize) + #add points colored by significance
     scale_color_manual(values = c("grey50","red")) + xlim(xlims) + ylim(ylims) +
     xlab("log2(fold change)") + ylab("-log10(FDR)") + theme_bw() + 
     theme(legend.position = "none", axis.text = element_text(size=axisLabelsize), axis.title = element_text(size=axisTitle,face="bold")) + #ggtitle("Volcanoplot")
