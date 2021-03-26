177a9bb7bbbae9f01437d3dfec968676d8c1c72f
Colin Tang <53540424+cot2005@users.noreply.github.com>
2021-03-26 08:54:43 -0700

    Update VolcanoPlot.R

--- VolcanoPlot.R
+++ VolcanoPlot.R
@@ -2,8 +2,7 @@ library("ggplot2")
 library("ggrepel")
 library("dplyr")
 
-
-#more refined volcano plot function used to plot and label a list of genes.
+# volcano plot function used to make custom volcano plots and label genes of interest.
 
 volcano.plot<-function(rnaseqdata, alphavalue = 0.05, lfcLim = 1.5, top = 10, genesetFile = NULL, graphname = "volcano",
                        xlims = c(-2.5,2.5), ylims = c(0,20), width = 6, height = 6, 
@@ -33,7 +32,7 @@ volcano.plot<-function(rnaseqdata, alphavalue = 0.05, lfcLim = 1.5, top = 10, ge
   v + geom_point(aes(col = sig, shape=shape)) + #add points colored by significance
     scale_color_manual(values = c("grey50","red")) + xlim(xlims) + ylim(ylims) +
     xlab("log2(fold change)") + ylab("-log10(FDR)") + theme_bw() + 
-    theme(legend.position = "none", axis.text = element_text(size=axisFontsize), axis.title = element_text(size=axisTitle,face="bold")) + #ggtitle("Volcanoplot")
+    theme(legend.position = "none", axis.text = element_text(size=axisLabelsize), axis.title = element_text(size=axisTitle,face="bold")) + #ggtitle("Volcanoplot")
     geom_text_repel(label = input$label, box.padding = boxpadding, size = labelSize, force = labelRepel, segment.size = 0.3, 
                     min.segment.length = 0.1, segment.alpha = 0.5, na.rm = TRUE, ylim = c(ylims[1],ylims[2])) #adding text for the top 20 genes
   ggsave(paste(graphname, "_Volcanoplot.pdf", sep = ""), width = width, height = height)
